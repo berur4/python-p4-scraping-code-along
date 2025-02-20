13ee77e0c32f50ca4e38ed1ffa2c8049831ebfd5
berur4 <berurcaleb@gmail.com>
2025-02-20 13:41:10 +0300

    Scraping

--- lib/testing/course_test.py
+++ lib/testing/course_test.py
@@ -1,21 +1,19 @@
-from Course import Course
+from Course import Course  # Make sure you're importing the correct class
 
 class Test_Course:
     '''Course in Course.py'''
 
-
-
     def test_title_instance_variable(self):
         "Test setting and getting for title"
-        course = Course(title="Programming Robots for Outer Space")
-        assert(course.title == "Programming Robots for Outer Space")
+        course = Course(title="Programming Robots for Outer Space", schedule="Full-Time", description="Learn how to program robots.")
+        assert course.title == "Programming Robots for Outer Space"
 
     def test_schedule_instance_variable(self):
         "Test setting and getting for schedule"
-        course = Course(schedule="Full-Time")
-        assert(course.schedule == "Full-Time")
+        course = Course(title="Programming Robots for Outer Space", schedule="Full-Time", description="Learn how to program robots.")
+        assert course.schedule == "Full-Time"
 
     def test_description_instance_variable(self):
         "Test setting and getting for description"
-        course = Course(description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
-        assert(course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+        course = Course(title="Programming Robots for Outer Space", schedule="Full-Time", description="Learn how to program robots.")
+        assert course.description == "Learn how to program robots."
